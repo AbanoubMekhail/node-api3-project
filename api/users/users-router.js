c97f185772c444ccd6230044018068275d243595
abanoub nagib <abanoub.nagib@hotmail.com>
2023-10-23 21:35:47 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const {
-  validateUserId,
+  validateUserId, validateUser, validatePost,
 } = require("../middleware/middleware")
+
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  console.log(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.name)
 });
 
-router.put('/:id', validateUserId,  (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -38,11 +48,21 @@ router.get('/:id/posts', validateUserId,  (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user)
+  console.log(req.text)
 });
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ 
+    cuwstomMessage: "somthing went wrong!!", 
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
 module.exports = router
